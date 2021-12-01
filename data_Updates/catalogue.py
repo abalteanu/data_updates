7007607b69803581101e07469f15e06e603d4bce
abalteanu <58431085+abalteanu@users.noreply.github.com>
2021-12-01 00:50:40 -0500

    Fixed bug in class 2
    Now I must make inherited class

--- data_Updates/catalogue.py
+++ data_Updates/catalogue.py
@@ -16,16 +16,16 @@ class CountryCatalogue:
             # if one of the files does not exist, return an empty list
             return self._countryCat
 
-        next(c_file) #skipping first line
+        next(c_file) # skipping first line
         for lineNum in c_file:
 
             temp = lineNum.strip("\n").split("|")
 
-            print(temp)
-            self._countryCat["countries"] = temp[0]
-            self._countryCat["continents"] = temp[1]
-            self._countryCat["populations"] = temp[2]
-            self._countryCat["areas"] = temp[3]
+            print(type(temp[1]))
+            self._countryCat["countries"].append(temp[0])
+            self._countryCat["continents"].append(temp[1])
+            self._countryCat["populations"].append(temp[2])
+            self._countryCat["areas"].append(temp[3])
 
     def getCountryList(self):
         return self._countryCat
@@ -45,13 +45,13 @@ class CountryCatalogue:
         # Method that prints a list of the countries and their info to the screen
         # use __repr__ from Country class
         for i in range(len(self._countryCat["countries"])):
-            print(i)
-            print(self._countryCat["countries"][0])
-            print("Country: ", self._countryCat["countries"[i]])
-            print("Continent: ", self._countryCat["continents"[i]])
-            print("Population: ", self._countryCat["populations"[i]])
-            print("Area: ", self._countryCat["areas"[i]])
-    #
+            print("Country #", i)
+            print("Country: ", self._countryCat["countries"][i])
+            print("Continent: ", self._countryCat["continents"][i])
+            print("Population: ", self._countryCat["populations"][i])
+            print("Area: ", self._countryCat["areas"][i])
+            print()
+
     # def saveCountryCatalogue(self, name):
     #     # Method that enables all the country info in the catalogue to be saved to a file
     #     # countries should be sorted alphabetically prior to saving
