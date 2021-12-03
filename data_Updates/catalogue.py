f98c7cfc0b9c5978b5f7f927d2e837496b5f2343
abalteanu <58431085+abalteanu@users.noreply.github.com>
2021-12-02 22:51:17 -0500

    Added setter methods for Country Catalogue

--- data_Updates/catalogue.py
+++ data_Updates/catalogue.py
@@ -1,6 +1,6 @@
 import country
 
-class CountryCatalogue():
+class CountryCatalogue:
     def __init__(self, countryFile):
 
         self._countryCat = []
@@ -10,7 +10,7 @@ class CountryCatalogue():
             c_file = open(countryFile, encoding='utf-8', errors='ignore')
         except IOError:
             # if one of the files does not exist, return an empty list
-            return self._countryCat
+            print("Error: file not accepted")
 
         next(c_file) # skipping first line
         for lineNum in c_file:
@@ -18,14 +18,37 @@ class CountryCatalogue():
             self._countryCat.append(country.Country(temp[0], temp[2], temp[3], temp[1]))
 
     def getCountryList(self):
+        # prints the country catalogue out to the screen very simply, without formatting
         return self._countryCat
 
+    def findCountryIndex(self, country):
+        # finds the index of a country (string) in countryCat
+        # if no country, returns None
+        for i in range(len(self._countryCat)):
+            if country == self._countryCat[i].getName():
+                return i
+            else:
+                return None
 
-    # def setPopulationOfCountry(self):
-    #
-    # def setAreaOfCountry(self):
-    #
-    # def setContinentOfCountry(self):
+    def setPopulationOfCountry(self, newPopulation, country):
+        # takes the new population to be changed and the country for which to change it and uses the findCountryIndex
+        # to find the index of the country in coutnryCat list, then uses the setPopulation method in Country class to
+        # assign the new population to the country
+        index = self.findCountryIndex(country)
+        if index != None:
+            self._countryCat[index].setPopulation(newPopulation)
+
+    def setAreaOfCountry(self, newArea, country):
+        # sets the Area of a specific country
+        index = self.findCountryIndex(country)
+        if index != None:
+            self._countryCat[country].setArea(newArea)
+
+    def setContinentOfCountry(self, newCont, country):
+        # sets the continent of a specific country
+        index = self.findCountryIndex(country)
+        if index != None:
+            self._countryCat[country].setContinent(newCont)
     #
     # def findCountry(self,country):
     #
@@ -35,6 +58,7 @@ class CountryCatalogue():
         # Method that prints a list of the countries and their info to the screen
         # use __repr__ from Country class
         for i in range(len(self._countryCat)):
+            # by printing the class, it calls the __repr__ method in the class Country
             print(self._countryCat[i])
 
     # def saveCountryCatalogue(self, name):
@@ -42,4 +66,8 @@ class CountryCatalogue():
     #     # countries should be sorted alphabetically prior to saving
 
 countries = CountryCatalogue("data.txt")
-print(countries.printCountryCatalogue())
+countries.printCountryCatalogue()
+print(countries.getCountryList())
+#print(countries.findCountryIndex("Brazil"))
+countries.setPopulationOfCountry(33, "Bazil")
+countries.printCountryCatalogue()
--- data_Updates/country.py
+++ data_Updates/country.py
@@ -3,10 +3,10 @@
 
 class Country:
     def __init__(self, name, pop, area, continent):
-        self._name = name
-        self._pop = pop
-        self._area = area
-        self._continent = continent
+        self._name = str(name)
+        self._pop = str(pop)
+        self._area = str(area)
+        self._continent = str(continent)
 
     def getName(self):
         return self._name
@@ -21,18 +21,19 @@ class Country:
         return self._continent
 
     def setPopulation(self, newPop):
-        self._pop = newPop
+        self._pop = str(newPop)
 
     def setArea(self, newArea):
-        self._area = newArea
+        self._area = str(newArea)
 
     def setContinent(self, newContinent):
         self._continent = newContinent
 
     def __repr__(self):
-        return self._name + " (pop: " + str(self._pop) + ", size: " + str(self._area) + ") in " + self._continent
+        return self._name + " (pop: " + self._pop + ", size: " + self._area + ") in " + self._continent
+
 
 # c1 = Country("Canada", 3000000, 60000000, "North_America")
 # print(c1)
-# c1.setContinent("blue")
+# c1.setPopulation("33")
 # print(c1)
